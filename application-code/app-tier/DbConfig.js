f8095746ddc412ff40ba2c7a4e0cfdcb38e455ce
Abhijeet <122082856+abhijeetpoonia@users.noreply.github.com>
2023-10-16 13:05:02 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : '13.208.155.63',
+    DB_USER : 'admin',
+    DB_PWD : 'mypassword',
+    DB_DATABASE : 'webappdb'
 });
